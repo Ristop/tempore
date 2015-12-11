bc85a72aefb1c343000ecbdd402f7f38342fc2cc
Anna Bilmaijer <anna@mooncascade.com>
2015-12-11 23:25:28 +0200

    MasterDetail view

---
+++ tempore/Data/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tempore.Data
+{
+     public class Item
+     {
+          public int Id { get; set; }
+          public int totalSeconds { get; set; }
+          public int remainingSeconds { get; set; }
+          public string Title { get; set; }
+          public string Text { get; set; }
+     }
+}
---
+++ tempore/ViewModels/ItemViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using tempore.Data;
+using Windows.Globalization.DateTimeFormatting;
+namespace tempore.ViewModels
+{
+     public class ItemViewModel
+     {
+          private int _itemId;
+
+          public int ItemId
+          {
+               get
+               {
+                    return _itemId;
+               }
+          }
+
+          public string DateCreatedHourMinute
+          {
+               get
+               {
+                    var formatter = new DateTimeFormatter("hour minute");
+                    return formatter.Format(DateCreated);
+               }
+          }
+
+          public string Title { get; set; }
+          public string Text { get; set; }
+          public DateTime DateCreated { get; set; }
+
+          public ItemViewModel()
+          {
+          }
+
+          public static ItemViewModel FromItem(Item item)
+          {
+               var viewModel = new ItemViewModel();
+
+               viewModel._itemId = item.Id;
+               viewModel.Title = item.Title;
+               viewModel.Text = item.Text;
+
+               return viewModel;
+          }
+     }
+}
